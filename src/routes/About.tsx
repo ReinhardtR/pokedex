afa5e8c109fd9000cff58a5352f30b17387ecc94
Reinhardt Rijna <69666626+ReinhardtR@users.noreply.github.com>
2023-04-16 21:33:02 +0200

    add about page

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,3 +1,13 @@
+import { Stack, Text } from "@mantine/core";
+
 export function About() {
-  return <div>About</div>;
+  return (
+    <Stack align="center" justify="center">
+      <Text size="xl">This is a Pokédex app built with React.</Text>
+      <Text size="xl">
+        It uses the PokéAPI GraphQL-endpoint to fetch data about Pokémon.
+      </Text>
+      <Text size="xl">UI is built with a mixture of Mantine and Tailwind.</Text>
+    </Stack>
+  );
 }
